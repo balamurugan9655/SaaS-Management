8196fa1cd24cc81ea9a34bbe4bc5a271ae2a6ffc
balamurugan9655 <shanmugambala80@gmail.com>
2025-06-18 12:53:54 +0530

    update commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const instance = axios.create({ baseURL: 'http://localhost:3500/api' });
+const instance = axios.create({ baseURL: 'https://saas-management-server-tzvg.onrender.com/api' });
 
 export default instance;
